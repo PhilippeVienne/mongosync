173b16dcac2cbcd009038093877999afbcf3d988
Philippe Vienne <philippe@skyloud.fr>
2024-08-14 10:29:45 +0200

    feat: add migration script

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM ubuntu:24.04
 WORKDIR /tmp
 ARG VERSION=1.8.0
-RUN apt update && apt install -y wget rsyslog-gssapi
+RUN apt update && apt install -y wget rsyslog-gssapi curl jq
 RUN wget https://fastdl.mongodb.org/tools/mongosync/mongosync-ubuntu2004-x86_64-${VERSION}.tgz && \
     tar -xzf mongosync-ubuntu2004-x86_64-${VERSION}.tgz && \
     cp mongosync-ubuntu2004-x86_64-${VERSION}/bin/mongosync /usr/local/bin/ && \
+    rm -rf mongosync-ubuntu2004-x86_64-${VERSION}.tgz mongosync-ubuntu2004-x86_64-${VERSION} && \
     mongosync -v
-ENTRYPOINT [ "mongosync" ]
+COPY ./migrate.sh /usr/local/bin/migrate.sh
+RUN chmod +x /usr/local/bin/migrate.sh
+CMD [ "/usr/local/bin/migrate.sh" ]
---
+++ migrate.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+set -e
+
+# This script is used to migrate the data from one server to another server
+# using mongosync tool.
+
+# Set the source and destination server details
+if [ -z "$MONGODB_CLUSTER0" ]; then
+  # Try to get cluster0 from /mnt/secrets/cluster0
+    if [ -f /mnt/secrets/cluster0 ]; then
+        MONGODB_CLUSTER0=$(cat /mnt/secrets/cluster0)
+    else
+        echo "MONGODB_CLUSTER0 is not set"
+        exit 1
+    fi
+fi
+
+if [ -z "$MONGODB_CLUSTER1" ]; then
+  # Try to get cluster1 from /mnt/secrets/cluster1
+    if [ -f /mnt/secrets/cluster1 ]; then
+        MONGODB_CLUSTER1=$(cat /mnt/secrets/cluster1)
+    else
+        echo "MONGODB_CLUSTER1 is not set"
+        exit 1
+    fi
+fi
+
+# Write migration configuration file
+cat <<EOF > /tmp/migrate.yaml
+cluster0: $MONGODB_CLUSTER0
+cluster1: $MONGODB_CLUSTER1
+verbosity: ${MIGRATE_VERBOSITY:-"WARN"}
+disableTelemetry: ${MIGRATE_DISABLE_TELEMETRY:-true}
+loadLevel: ${MIGRATE_LOAD_LEVEL:-"3"}
+port: ${MIGRATE_PORT:-"27182"}
+EOF
+
+mongosync --config /tmp/migrate.yaml &
+
+start_migration() {
+    echo "Starting migration"
+    start=$(curl -X POST http://localhost:${MIGRATE_PORT:-"27182"}/api/v1/start -H "Content-Type: application/json" -d '{"source": "cluster0","destination": "cluster1"}')
+
+    success=$(echo $start | jq -r '.success')
+    if [ "$success" != "true" ]; then
+    echo "Failed to start migration"
+    echo $start
+    exit 1
+    fi
+}
+
+echo "Waiting for mongosync to start"
+while true; do
+  if curl -s http://localhost:${MIGRATE_PORT:-"27182"}/api/v1/progress > /dev/null; then
+    progress=$(curl -s http://localhost:${MIGRATE_PORT:-"27182"}/api/v1/progress | jq -r '.progress')
+    state=$(echo $progress | jq -r '.state')
+    if [ "$state" == "IDLE" ]; then
+        echo "First time starting migration"
+        start_migration
+    fi
+  fi
+  sleep 1
+done
+
+
+check_progress() {
+  while true; do
+    progress=$(curl -s http://localhost:${MIGRATE_PORT:-"27182"}/api/v1/progress | jq -r '.progress')
+    echo "Migration progress: $(echo $progress | jq -r '.state') - $(echo $progress | jq -r '.info')"
+    canCommit=$(echo $progress | jq -r '.canCommit')
+    if [ "$canCommit" == "true" ]; then
+      echo "Migration completed"  
+    fi
+    sleep 5
+  done
+}
+
+check_progress
