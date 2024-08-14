754f79b516f265e98ac9dc4c430d31f9121c14d7
Philippe Vienne <philippe@skyloud.fr>
2024-08-14 11:55:54 +0200

    feat: add nginx proxy

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM ubuntu:24.04
 WORKDIR /tmp
 ARG VERSION=1.8.0
-RUN apt update && apt install -y wget rsyslog-gssapi curl jq
+ENV NGINX_ENABLED=1
+RUN apt update && apt install -y wget rsyslog-gssapi curl jq nginx
 RUN wget https://fastdl.mongodb.org/tools/mongosync/mongosync-ubuntu2004-x86_64-${VERSION}.tgz && \
     tar -xzf mongosync-ubuntu2004-x86_64-${VERSION}.tgz && \
     cp mongosync-ubuntu2004-x86_64-${VERSION}/bin/mongosync /usr/local/bin/ && \
     rm -rf mongosync-ubuntu2004-x86_64-${VERSION}.tgz mongosync-ubuntu2004-x86_64-${VERSION} && \
     mongosync -v
+COPY ./nginx.conf /etc/nginx/sites-available/default
 COPY ./migrate.sh /usr/local/bin/migrate.sh
 RUN chmod +x /usr/local/bin/migrate.sh
 CMD [ "/usr/local/bin/migrate.sh" ]
--- migrate.sh
+++ migrate.sh
@@ -36,53 +36,53 @@ loadLevel: ${MIGRATE_LOAD_LEVEL:-"3"}
 port: ${MIGRATE_PORT:-"27182"}
 EOF
 
+# Start NGINX in the background if NGINX_ENABLED
+if [ "$NGINX_ENABLED" == "true" ]; then
+    echo "Starting NGINX"
+    nginx -g "daemon off;" &
+    NGINX_PID=$!
+    trap "kill $NGINX_PID" EXIT
+fi
+
 mongosync --config /tmp/migrate.yaml &
 
 MONGOSYNC_PID=$!
 
 trap "kill $MONGOSYNC_PID" EXIT
 
-start_migration() {
-    echo "Starting migration"
-    start=$(curl -X POST http://localhost:${MIGRATE_PORT:-"27182"}/api/v1/start -H "Content-Type: application/json" -d '{"source": "cluster0","destination": "cluster1"}')
-
-    success=$(echo $start | jq -r '.success')
-    if [ "$success" != "true" ]; then
-        echo "Failed to start migration"
-        echo $start
-        exit 1
-    fi
-}
-
 echo "Waiting for mongosync to start"
+
 while true; do
   if curl -s http://localhost:${MIGRATE_PORT:-"27182"}/api/v1/progress > /dev/null; then
     progress=$(curl -s http://localhost:${MIGRATE_PORT:-"27182"}/api/v1/progress | jq -r '.progress')
     state=$(echo $progress | jq -r '.state')
     if [ "$state" == "IDLE" ]; then
-        echo "First time starting migration"
-        start_migration
+        echo "Waiting for migration to start, you can run ' curl -X POST http://localhost:${MIGRATE_PORT:-"27182"}/api/v1/start -H \"Content-Type: application/json\" -d '{\"source\": \"cluster0\",\"destination\": \"cluster1\"}' ' to start the migration"
     fi
-    if [ "$state" == "COMMITED" ]; then
+    if [ "$state" == "COMMITTED" ]; then
         echo "Migration has already been completed"
         exit 0
     fi
+    if [ "$state" == "RUNNING" ]; then
+        echo "Migration is progressing"
+        echo "Migration progress: $(echo $progress | jq -r '.state') - $(echo $progress | jq -r '.info')"
+        canCommit=$(echo $progress | jq -r '.canCommit')
+        if [ "$canCommit" == "true" ]; then
+            echo "Migration can be committed, you can run ' curl -X POST http://localhost:${MIGRATE_PORT:-"27182"}/api/v1/commit -d '{}' ' to commit the migration"
+        fi
+    fi
+    if [ "$state" == "COMMITTING" ]; then
+        echo "Migration is committing"
+        echo "Migration progress: $(echo $progress | jq -r '.state') - $(echo $progress | jq -r '.info')"
+    fi
+    if [ "$state" == "PAUSED" ]; then
+        echo "Migration is paused"
+        echo "Migration progress: $(echo $progress | jq -r '.state') - $(echo $progress | jq -r '.info')"
+    fi
+  else
+    echo "Waiting for mongosync to start (note that it may take a few minutes to start on retries)"
   fi
-  echo "Waiting for mongosync to start"
   sleep 1
 done
 
 
-check_progress() {
-  while true; do
-    progress=$(curl -s http://localhost:${MIGRATE_PORT:-"27182"}/api/v1/progress | jq -r '.progress')
-    echo "Migration progress: $(echo $progress | jq -r '.state') - $(echo $progress | jq -r '.info')"
-    canCommit=$(echo $progress | jq -r '.canCommit')
-    if [ "$canCommit" == "true" ]; then
-      echo "Migration completed"  
-    fi
-    sleep 5
-  done
-}
-
-check_progress
